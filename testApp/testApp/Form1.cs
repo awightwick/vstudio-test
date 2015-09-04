bfe5ccd946dcaf6002ec485f90aa07f05e1abf34
Andy Wightwick <andrew@wightwick.com>
2015-09-04 15:02:38 -0400

    Updating this branch as a test

--- testApp/testApp/Form1.cs
+++ testApp/testApp/Form1.cs
@@ -16,5 +16,10 @@ namespace testApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //here too?
+        }
     }
 }
